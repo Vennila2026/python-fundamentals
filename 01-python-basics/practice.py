3a891f00293775a37e70e10f33aba2b8a1fdf69e
Vennila <itsvennila.m@gmail.com>
2026-08-23 23:45:11 +0530

    Implement addition and division operations
    Added a program to perform addition and division operations based on user input.

--- 01-python-basics/practice.py
+++ 01-python-basics/practice.py
@@ -6,3 +6,39 @@ age =  "14"
 
 print("Name:", name)
 print("Age:", age)
+----------------------------------------------------------------------------
+# Write a python program to do arithmetical operations addition and divistion.
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the last number: "))
+
+# Addition
+sum = num1 + num2
+print(f"sum: {num1} + {num2} = {sum}")
+
+# Division
+if num2 != 0:
+  print("Error: Division by zero is not allowed")
+else:
+  division = num1/num2
+  print(f"Division: {num1}/{num2} = {division}")
+---------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
